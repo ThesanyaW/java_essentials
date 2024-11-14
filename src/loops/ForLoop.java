5453d5dff1710359d76b2bbc1cd25ff116ec40e6
Thesanya <thesanyawijayasundara490@gmail.com>
2024-11-14 16:36:24 +0530

    add ForLoop class

---
+++ src/loops/ForLoop.java
@@ -0,0 +1,27 @@
+package loops;
+
+import java.util.Scanner;
+
+public class ForLoop {
+
+    public static void main(String[] args){
+
+        int i, quantity;
+        double price, total = 0;
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Please enter the item quantity: ");
+        quantity = scanner.nextInt();
+
+        for(i=0; i<quantity; i++){
+
+            System.out.println("Please enter the item price: ");
+            price = scanner.nextDouble();
+
+            total = total + price;
+        }
+
+        System.out.println("Total cost is " + total + "$");
+    }
+}
